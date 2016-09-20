48550cfa896eae17983ee2ba969e12b2dcdfa11f
Jazzy Cordaro <jazzy_cordaro@hotmail.com>
2016-09-20 16:17:00 -0500

    add pic background

--- angular.js
+++ angular.js
@@ -1,1 +1,8 @@
 console.log('angular.js is sourced yo');
+
+var myApp = angular.module('myApp', []);
+
+var allFruits=[];
+myApp.controller('jazzy', ['$scope', function($scope){
+  console.log('ng');
+}]);
